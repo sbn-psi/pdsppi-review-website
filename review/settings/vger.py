63c2a319d5b276e21bf1873c4caf6cf42dc6868b
Conor Kingston <cmkingston21@me.com>
2022-11-03 15:12:05 -0700

    add vger.psi.edu to allowed hosts

--- review/settings/vger.py
+++ review/settings/vger.py
@@ -7,7 +7,7 @@ DEBUG = os.environ.get("DEBUG")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['vger.psi.edu:3434'] 
+ALLOWED_HOSTS = ['vger.psi.edu', 'vger.psi.edu:3434'] 
 
 DATABASES = {
     'default': {
