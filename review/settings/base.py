9d52bb2e0a68bb0633ca7355d5bd3493ba29f403
Conor Kingston <cmkingston21@me.com>
2023-04-06 16:57:25 -0700

    store static/media files in s3 and host as cdn using cloudflare

--- review/settings/base.py
+++ review/settings/base.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'missions',
     'django_comments_xtd',
     'django_comments',
+    'storages',
 
     'wagtail.contrib.forms',
     'wagtail.contrib.modeladmin',
@@ -186,3 +187,29 @@ COMMENTS_XTD_MAX_THREAD_LEVEL = 5
 COMMENTS_XTD_LIST_ORDER = ('-thread_id', 'order')
 
 CSRF_TRUSTED_ORIGINS = ['https://review-service.u4m6gudpq07vk.us-west-2.cs.amazonlightsail.com']
+
+# AWS S3, Cloudflare CDN configuration
+# stores/serves static and media directories in AWS in production
+USE_S3 = os.getenv('DEBUG') == 'False'
+
+if USE_S3:
+    # Upload media files to S3
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_DEFAULT_ACL = 'public-read'
+    # Using S3 as a CDN (via CloudFront), tell storage to serve files from there
+    AWS_S3_CUSTOM_DOMAIN = 'd3qrohnnpazynl.cloudfront.net'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    AWS_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    # Allow `django-admin collectstatic` to automatically put your static files in your bucket
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3ManifestStaticStorage'
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
